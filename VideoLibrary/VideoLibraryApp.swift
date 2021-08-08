371bad26fab132a25f696d9e7b032dba8096214b
Kevin Tooley <kevin.tooley@snhu.edu>
2021-08-08 18:12:01 -0400

    Initial Commit

---
+++ VideoLibrary/VideoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLibraryApp.swift
+//  VideoLibrary
+//
+//  Created by Kevin Tooley on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
